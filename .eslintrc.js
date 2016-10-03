5e969a43fe2ef0a2ac7233acb327459312c2b56e
zhouzhihu <web_study2004@163.com>
2016-10-03 13:48:06 +0800

    add new rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,6 +16,7 @@ module.exports = {
     'arrow-parens': 0,
     // allow async-await
     'generator-star-spacing': 0,
+    'no-new' : 0,
     // allow debugger during development
     'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
   }
