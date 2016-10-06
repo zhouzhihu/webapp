5658108baf0c0cc40f3feafa3a9264135fa4958a
zhouzhihu <web_study2004@163.com>
2016-10-07 02:34:46 +0800

    add css

--- src/module/index/app.js
+++ src/module/index/app.js
@@ -1,8 +1,9 @@
 import Vue from 'vue'
+import App from './App.vue'
 import router from '../router/router.js'
 
 const app = new Vue({
   router,
-  template: '<div id="app"><router-link to="/">home</router-link><router-link to="/about">about</router-link><transition name="fade" mode="out-in"><router-view class="view"></router-view></transition></div>'
+  ...App
 }).$mount("#app")
 export { app}
