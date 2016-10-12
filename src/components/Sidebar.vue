93e3b04ce65a3000973b00065761dfa1a7c2104d
zhouzhihu <web_study2004@163.com>
2016-10-12 13:48:24 +0800

    add prototype

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -53,18 +53,6 @@
             <span>测试</span>
             <div>+</div>
           </li>
-          <li class="sidebar-list-li">
-            <span>测试</span>
-            <div>+</div>
-          </li>
-          <li class="sidebar-list-li">
-            <span>测试</span>
-            <div>+</div>
-          </li>
-          <li class="sidebar-list-li">
-            <span>测试</span>
-            <div>+</div>
-          </li>
         </ul>
       </div>
     </div>
@@ -82,93 +70,117 @@
   }
 </script>
 
-<style scoped lang="stylus">
-  .iconfont
-    font-family:"iconfont"
-    font-size: 14px
-    font-style:normal
-    color: #ffffff
-  .show-sidebar
-    -webkit-transform: translateX(290px)
-  .sidebar-box
-    position: fixed
-    -webkit-transform: translateZ(0)
-    height: 100%
-    overflow: auto
-    width: 290px
-    left: -290px
-    z-index: 99
-    top: 0
-    bottom: 0
-    transition: all .3s ease
-    &.show-sidebar
-      -webkit-transform: translateX(290px)
-    .swiper-slide
-      font-size: 15px
-      height: auto
-      -webkit-box-sizing: border-box
-      box-sizing: border-box
-  .sidebar-box
-    .sidebar-header
-      padding: 15px
-      background: #00A2EA
-      .user
-        display: flex
-        flex-direction: row
-        img
-          width: 30px
-          height: 30px
-          border-radius: 50%
-        span
-          margin-left: 20px
-          line-height: 30px
+<style scoped lang="scss" rel="stylesheet/scss">
+  .iconfont {
+    font-family:"iconfont";
+    font-size: 14px;
+    font-style:normal;
+    color: #ffffff;
+  }
+
+  .show-sidebar{
+    transform: translateX(240px);
+    -webkit-transform: translateX(240px);
+  }
+  .sidebar-box{
+    position: fixed;
+    transform: translateZ(0);
+    -webkit-transform: translateZ(0);
+    height: 100%;
+    overflow: auto;
+    width: 240px;
+    left: -240px;
+    z-index: 99;
+    top: 0;
+    bottom: 0;
+    transition: all .3s ease;
+    &.show-sidebar{
+      transform: translateX(240px);
+      -webkit-transform: translateX(240px);
+    }
+    .swiper-slide {
+      font-size: 15px;
+      height: auto;
+      -webkit-box-sizing: border-box;
+      box-sizing: border-box;
+    }
+  }
+  .sidebar-box{
+    .sidebar-header{
+      padding: 15px;
+      background: #00A2EA;
+      .user{
+        display: flex;
+        flex-direction: row;
+        >img{
+          width: 30px;
+          height: 30px;
+          border-radius: 50%;
+        }
+        >span{
+          margin-left: 20px;
+          line-height: 30px;
           flex: 1;
-          font-size: 18px
-          font-weight: 300
-          color: #ffffff
-    .function
-      margin-top: 25px
-      display: flex
-      flex-direction: row
-      color: #ffffff
-      .function-sub
-        padding-left: 52px
-        flex: 1
-        font-size: 13px
-        font-weight: 300
-        position: relative
-        i
-          position: absolute
-          top: 0
-          left: 20px
-  .sidebar-list
-    padding-bottom: 20px
-    background: #f9f9f9
-    .sidebar-list-first
-      background: #f9f9f9
-      color: #00A2EA
-      height: 35px
-      line-height: 35px
-      font-size: 15px
-      padding-left: 42px
-      position: relative
-      i
-        color: #00A2EA
-        font-size: 20px
-        position: absolute
-        left: 20px
-    .sidebar-list-li
-      height: 47px
-      line-height: 47px
-      display: flex
-      flex-direction: row
-      padding-left: 15px
-      span
-        width: 200px
-      div
-        padding-right: 40px
-        font-size: 25px
-        font-weight: 200
-        color: #868686
+          font-size: 18px;
+          font-weight: 300;
+          color: #ffffff;
+        }
+      }
+    }
+    .function{
+      margin-top: 25px;
+      display: inline-flex;
+      flex-direction: row;
+      color: #ffffff;
+      .function-sub{
+        padding-left: 22px;
+        flex: 1;
+        font-size: 13px;
+        font-weight: 300;
+        position: relative;
+        >i{
+          position: absolute;
+          top: 0;
+          left: 5px;
+        }
+      }
+    }
+  }
+  .sidebar-list{
+    padding-bottom: 20px;
+    background: #f9f9f9;
+    .sidebar-list-first{
+      background: #f9f9f9;
+      color: #00A2EA;
+      height: 47px;
+      line-height: 47px;
+      font-size: 15px;
+      padding-left: 55px;
+      position: relative;
+      >i{
+        color: #00A2EA;
+        font-size: 20px;
+        position: absolute;
+        top: 0px;
+        left: 20px;
+      }
+    }
+    .sidebar-list-li{
+      height: 47px;
+      line-height: 47px;
+      display: flex;
+      flex-direction: row;
+      padding-left: 15px;
+      >p{
+        flex: 1;
+      }
+      >div{
+        padding-right: 40px;
+        font-size: 25px;
+        font-weight: 200;
+        color: #868686;
+      }
+    }
+  }
 </style>
 
