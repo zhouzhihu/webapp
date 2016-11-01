f687af2f71262b76ad1465500e46c6b2d2a17232
zhouzhihu <web_study2004@163.com>
2016-11-01 20:12:38 +0800

    add search demo

--- src/module/addressList/index.vue
+++ src/module/addressList/index.vue
@@ -1,23 +1,23 @@
 <template>
     <div class="news-view">
-      <div class="news-list-nav" mode="out-in">
-        <div v-if="!search" class="nav">
-          <router-link to="/AddressList/all">所有联系人</router-link>
-          <router-link to="/AddressList/favorite">我收藏的联系人</router-link>
-          <div class="icon">
-            <i class="el-icon-plus"></i>
-          </div>
-          <div class="icon" @click="toggleSearch">
-            <i class="el-icon-search"></i>
-          </div>
-        </div>
-        <div v-if="search" class="nav">
-          <div class="input">
-            <input type="text" placeholder="请输入要搜索联系人" />
-          </div>
-          <div @click="toggleSearch">
-            <i class="el-icon-search"></i>
-          </div>
+      <div class="news-list-nav">
+        <div class="nav">
+            <div class="main" v-if="!search">
+              <router-link to="/AddressList/all">所有联系人</router-link>
+              <router-link to="/AddressList/favorite">我收藏的联系人</router-link>
+              <div class="icon">
+                <i class="el-icon-plus"></i>
+              </div>
+              <div class="icon" @click="toggleSearch">
+                <i class="el-icon-search"></i>
+              </div>
+            </div>
+            <div v-if="search">
+              <el-row :gutter="5">
+                <el-col :span="18"><el-input placeholder="请输入要搜索联系人"></el-input></el-col>
+                <el-col :span="6"><div @click="toggleSearch"><el-button type="primary" icon="circle-cross">取消</el-button></div></el-col>
+              </el-row>
+            </div>
         </div>
       </div>
       <transition name="fade" mode="out-in">
@@ -27,9 +27,16 @@
 </template>
 
 <script>
+  import {Button as ElButton, Input as ElInput, Row as ElRow, Col as ElCol} from 'element-ui'
   export default {
     beforeMount(){
       this.$Progress.start()
+    },
+    components:{
+      ElButton,
+      ElInput,
+      ElRow,
+      ElCol
     },
     mounted(){
       this.$Progress.finish()
@@ -56,7 +63,7 @@
     .news-view
       padding-top 45px
     .news-list-nav
-      padding 15px 10px
+      padding 3px 10px 3px 3px
       position fixed
       text-align center
       top 55px
@@ -65,14 +72,6 @@
       z-index 3
       box-shadow 0 1px 2px rgba(0,0,0,.1)
       background-color #58B7FF
-      .fade-enter-active
-        transition opacity .5s
-      .fade-leave-active
-        transition opacity .5s
-      .fade-enter
-        opacity 0
-      .fade-leave-active
-        opacity 0
       .nav
         max-width 800px
         box-sizing border-box;
@@ -87,26 +86,11 @@
           font-weight 400
         .disabled
           color #ccc
-        div
-          display inline
-        .icon
-          color #fff
-          float right
-          margin-left 10px
-          font-size 1.2em
-        input
-          display block
-          padding 3px 10px
-          box-sizing border-box
-          width 100%
-          height 28px
-          color #1f2d3d
-          background-color #fff
-          background-image none
-          border 1px solid #c0ccda
-          border-radius 4px
-          -webkit-transition border-color .2s cubic-bezier(.645,.045,.355,1)
-          transition border-color .2s cubic-bezier(.645,.045,.355,1)
-          outline none
-          line-height normal
+        .main
+          padding 10px 10px
+          .icon
+            color #fff
+            float right
+            margin-left 10px
+            font-size 1.2em
 </style>
