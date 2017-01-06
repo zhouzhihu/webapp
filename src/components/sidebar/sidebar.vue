f47502b28ff1c02b2dccb23a7f88c3c5833ce228
zhouzhihu <web_study2004@163.com>
2017-01-06 15:11:33 +0800

    modify sass to css and modify index.html

--- src/components/sidebar/sidebar.vue
+++ src/components/sidebar/sidebar.vue
@@ -26,43 +26,40 @@
   }
 </script>
 
-<style scoped lang="scss" rel="stylesheet/scss">
+<style scoped lang="css">
   .show-sidebar{
-    transform: translateX(240px);
-    -webkit-transform: translateX(240px);
+    transform:translateX(240px);
+    -webkit-transform:translateX(240px);
   }
   .sidebar-box{
-    position: fixed;
-    transform: translateZ(0);
-    -webkit-transform: translateZ(0);
-    height: 100%;
-    overflow: auto;
-    width: 240px;
-    left: -240px;
-    z-index: 99;
-    top: 0;
-    bottom: 0;
-    transition: all .3s ease;
-    background: #fff;
-    &.show-sidebar{
-      transform: translateX(240px);
-      -webkit-transform: translateX(240px);
-    }
-    .swiper-slide {
-      height: auto;
-      -webkit-box-sizing: border-box;
-      box-sizing: border-box;
-    }
+    position:fixed;
+    transform:translateZ(0);
+    -webkit-transform:translateZ(0);
+    height:100%;
+    overflow:auto;
+    width:240px;
+    left:-240px;
+    z-index:99;
+    top:0;
+    bottom:0;
+    transition:all .3s ease;
+    background:#fff;
   }
-  .sidebar-box{
-    .sidebar-header{
-      padding: 15px;
-      background: #20A0FF;
-    }
+  .sidebar-box.show-sidebar{
+    transform:translateX(240px);
+    -webkit-transform:translateX(240px);
+  }
+  .sidebar-box .swiper-slide{
+    height:auto;
+    box-sizing:border-box;
+  }
+  .sidebar-box .sidebar-header{
+    padding:15px;
+    background:#20A0FF;
   }
   .sidebar-list{
-    padding-bottom: 20px;
-    background: #f9f9f9;
+    padding-bottom:20px;
+    background:#f9f9f9;
   }
 </style>
 
